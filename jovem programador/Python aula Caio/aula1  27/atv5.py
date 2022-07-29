917ac502c365ab98684b88f7232467e2ca0abb3d
Monique Ferreira <86627670+MoniqueFerreira@users.noreply.github.com>
2022-07-29 19:03:34 -0300

    Add files via upload

---
+++ jovem programador/Python aula Caio/aula1  27/atv5.py
@@ -0,0 +1,4 @@
+#Faça um Programa que converta metros para centímetros.
+metros= int(input("Digite os metros que quer converter para centimetros: "))
+centimetros=metros*100
+print(metros, "metros é igual a ", centimetros, "centimetros")
---
+++ jovem programador/Python aula Caio/aula1  27/exe3.py
@@ -0,0 +1,12 @@
+#pedir 2 números e falar se x é meior que y se x é menor que y ou se x é igual a y
+x=input('Digite um número:_')
+y=input('Digite outro número:_')
+
+if x > y:
+    print(x , 'é maior que',y)
+elif x<y:
+    print(x, 'é menor que',y)
+else:
+    print(x,'é igual a',y)
+
+
