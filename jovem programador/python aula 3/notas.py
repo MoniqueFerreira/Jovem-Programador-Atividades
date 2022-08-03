2414df483090a1f6b7110a456634494dbcc8c89f
Monique Ferreira <86627670+MoniqueFerreira@users.noreply.github.com>
2022-08-03 19:03:39 -0300

    up

---
+++ jovem programador/python aula 3/notas.py
@@ -0,0 +1,14 @@
+n=int(input('quantas notas vc quer calcular?_ '))
+media=0
+i=0
+
+for i in range((0,n)):
+    n = float(input(f'Digite a nota {i}'))
+    media += n
+print(f'sua média é {media/n}')
+
+       
+
+    
+    
+
---
+++ jovem programador/python aula 3/notas2.py
@@ -0,0 +1,5 @@
+qtas_notas=int(input('Quantas notas vc quer calcular? '))
+contador=0
+while contador <= qtas_notas:
+    notas=input('Digite sua nota: ')
+
